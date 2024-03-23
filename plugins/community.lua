3d749388daa84393364374fe72b42afbc28f9aee
WillKirkmanM <willkirkmanmoller@gmail.com>
2024-03-23 19:46:10 +0000

    Feat(community.lua): Add Language Packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,14 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
